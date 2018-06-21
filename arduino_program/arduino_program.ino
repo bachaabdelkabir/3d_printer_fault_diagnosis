2addce6a54254f5ad4ea7f6d7ffe88bdb6ddc537
Abdelkabir BACHA <bachaabdelkabir@gmail.com>
2018-06-21 11:34:07 +0100

    removing time setting up from arduino program
    removing time setting up from arduino program and adding a new dataset

--- arduino_program/arduino_program.ino
+++ arduino_program/arduino_program.ino
@@ -124,7 +124,7 @@ void setup() {
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
     // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+//    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
     // This line sets the RTC with an explicit date & time, for example to set
     // January 21, 2014 at 3am you would call:
     // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
@@ -418,6 +418,7 @@ Vcc = readVcc()/1000.0;
   Serial.print(digitalRead(Y_EN));
 
 //---------------------------RTC section-------------------------------
+  Serial.print("\t"); 
   Serial.print(now.year(), DEC);
   Serial.print('/');
   Serial.print(now.month(), DEC);
